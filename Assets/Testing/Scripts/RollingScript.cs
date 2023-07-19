f33c1e145b3ac41f81116580f450888b9cdf5155
bean00173 <113889619+bean00173@users.noreply.github.com>
2023-07-20 10:46:09 +1200

    cross referencing old project

--- Assets/Testing/Scripts/RollingScript.cs
+++ Assets/Testing/Scripts/RollingScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RollingScript : MonoBehaviour
+public class DashingScript : MonoBehaviour
 {
     [Header("References")]
     public Transform orientation;
