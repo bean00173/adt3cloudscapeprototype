c1360f2158b3c1ff9e9fa7dc6d5ad6b51f758618
bean00173 <113889619+bean00173@users.noreply.github.com>
2023-07-27 18:41:49 +1200

    Physics Layer for no limb collisions

--- Assets/BodyPartContainer.cs
+++ Assets/BodyPartContainer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BodyPartContainer : MonoBehaviour
 {
 
-    public GameObject limb;
+    public GameObject[] limbs;
     // Start is called before the first frame update
     void Start()
     {
