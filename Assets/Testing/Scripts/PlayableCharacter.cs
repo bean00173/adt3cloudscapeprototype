ae0b40197b56c218935daf82a71ec4028dd21308
bean00173 <113889619+bean00173@users.noreply.github.com>
2023-08-08 15:43:05 +1200

    cant get health bc delay in start (static tower enter)

--- Assets/Testing/Scripts/PlayableCharacter.cs
+++ Assets/Testing/Scripts/PlayableCharacter.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayableCharacter : MonoBehaviour
@@ -16,11 +17,16 @@ public class PlayableCharacter : MonoBehaviour
     private void Start()
     {
         manager = CharacterManager.instance; // gathers instance of character manager
+        GetStats();
 
-        
     }
 
     private void Update()
+    {
+        GetStats();
+    }
+
+    public void GetStats()
     {
         healthModifier = manager.GetCurrentCharacter(currentCharacter).health; // returns stats from class manager method to return current character based on enum ID
         attackModifier = manager.GetCurrentCharacter(currentCharacter).atkMultiplier;
