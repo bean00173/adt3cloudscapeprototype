24838ae364bc5ee1f54596d2643ea0c2bf3e1f13
bean00173 <113889619+bean00173@users.noreply.github.com>
2023-08-08 15:48:09 +1200

    health needs to wait until scene loading

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -13,22 +13,22 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        healthBar.localScale = Vector3.one;
-        health = this.GetComponent<PlayableCharacter>().healthModifier;
-        currentHealth = health;
+        //healthBar.localScale = Vector3.one;
+        //health = this.GetComponent<PlayableCharacter>().healthModifier;
+        //currentHealth = health;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        float healthScale = currentHealth / health;
-        //healthBar.localScale = new Vector3(1f, healthScale, 1f);
+        //float healthScale = currentHealth / health;
+        ////healthBar.localScale = new Vector3(1f, healthScale, 1f);
 
-        if(currentHealth <= 0)
-        {
-            Debug.Log("LOSE");
-        }
+        //if(currentHealth <= 0)
+        //{
+        //    Debug.Log("LOSE");
+        //}
     }
 
     public void takeDamage(float damage)
