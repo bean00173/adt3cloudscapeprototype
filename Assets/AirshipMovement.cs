f6496f58945277e79670d3d19d8cbd6a75b55d00
bean00173 <113889619+bean00173@users.noreply.github.com>
2023-08-18 22:15:56 +1200

    Airship Movement Animations

--- Assets/AirshipMovement.cs
+++ Assets/AirshipMovement.cs
@@ -10,12 +10,14 @@ public class AirshipMovement : MonoBehaviour
     float hor, vert;
 
     Rigidbody rb;
+    Animator ac;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        ac = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -23,6 +25,8 @@ public class AirshipMovement : MonoBehaviour
     {
         hor = Input.GetAxisRaw("Horizontal");
         vert = Input.GetAxisRaw("Vertical");
+
+        ac.SetFloat("horizontalInput", hor);
 
         DoMove();
         //DoRot();
@@ -43,17 +47,30 @@ public class AirshipMovement : MonoBehaviour
 
         //// Adding Force to the Player Rigidbody if on Ground
         //rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        if(vert != 0)
+        {
+            ac.SetBool("moving", true);
+        }
+        else
+        {
+            ac.SetBool("moving", false);
+        }
+
         transform.Rotate(0, hor * 10 * Time.deltaTime, 0);
         transform.Translate(0, 0, vert * 10 * Time.deltaTime * moveSpeed);
+
+        ac.SetFloat("elevation", 0);
 
         if (Input.GetKey(KeyCode.Space)&& transform.position.y < 50)
         {
             transform.Translate(0, 10 * Time.deltaTime * moveSpeed, 0);
+            ac.SetFloat("elevation", 1);
 
         }
         else if (Input.GetKey(KeyCode.C) && transform.position.y > -50)
         {
             transform.Translate(0, -10 * Time.deltaTime * moveSpeed, 0);
+            ac.SetFloat("elevation", -1);
         }
 
     }
