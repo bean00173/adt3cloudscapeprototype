4fd0a382485c331ac8ec96a5756a245f5322b2c4
bean00173 <113889619+bean00173@users.noreply.github.com>
2023-09-22 23:13:04 +1200

    RigidBody Explosion soon to replace limb spawn

---
+++ Assets/TempExplosion.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempExplosion : MonoBehaviour
+{
+
+    public float explosionForce;
+    public float radius;
+    public float upwardsForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Explode());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(5f);
+
+        this.GetComponent<Collider>().enabled = false;
+        this.GetComponent<Rigidbody>().useGravity = false;
+
+        Collider[] colliders = GetComponentsInChildren<Collider>(true);
+
+        Debug.Log($"{name} {GetInstanceID()} has {colliders.Length} children colliders.");
+
+        foreach (Collider hit in colliders)
+        {
+            hit.enabled = true;
+
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            rb.isKinematic = false;
+            rb.useGravity = true;
+
+            rb.AddExplosionForce(explosionForce, this.transform.root.position, radius, upwardsForce);
+
+            Debug.Log($"Added Explosion Force to {rb.name}.");
+        }
+    }
+}
